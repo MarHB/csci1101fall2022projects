9e4748ebe6ac8138605e42e5132a687e5606e172
MarHB <mariohbarrera1@gmail.com>
2022-11-21 13:31:02 -0600

    Added files to games data

---
+++ games-data/sandbox crud operations query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int64_value,bool_value,double_value) VALUES (456789123369258147, 1, 123.456);
+
+--UPDATE sandbox SET string_value = 'THE ONE PIECE IS REAL' WHERE id = 14;
+
+--DELETE FROM sandbox WHERE id = 8;
